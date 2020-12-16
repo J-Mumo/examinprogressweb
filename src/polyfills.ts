9bfccbcd379c97219a2b9586956744a2871b6539
jmumo <jaymumo6@gmail.com>
2020-12-16 05:20:54 +0300

    upgraded to angular 11

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -65,4 +65,3 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-global.Buffer = global.Buffer || require('buffer').Buffer;
